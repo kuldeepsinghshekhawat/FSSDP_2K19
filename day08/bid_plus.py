19e0b855220f118034298d465e39a138a1b1be21
kuldeepsinghshekhaawt <kdbannachirana101@gmail.com>
2019-06-14 12:59:07 +0530

    update bid_plus program

--- day08/bid_plus.py
+++ day08/bid_plus.py
@@ -7,57 +7,53 @@ Created on Thu Jun 13 09:00:46 2019
 
 from  selenium import webdriver
 import pandas as pd
-#from selenium import webdriver
 from collections import OrderedDict
 from time import sleep
-from bs4 import BeautifulSoup as BS
+#from bs4 import BeautifulSoup as BS
 url = "https://bidplus.gem.gov.in/bidlists"
-browser = webdriver.Firefox("geckodriver.exe")
+browser = webdriver.Chrome("chromedriver.exe")
 browser.get(url)
 sleep(2)
-get_bid_result = browser.find_element_by_xpath('//*[@id="pagi_content"]/div[2]/div[1]')
-get_bid_result.click()
+for i in range(1,11):
+    get_bid_result = browser.find_element_by_xpath('//*[@id="pagi_content"]/div['+str(i)+']/div[1]/p[1]')
+    get_bid_result.click()
 
 
 sleep(5)
- 
-html_page = browser.page_source
-
-soup = BS(html_page)
-
-
-sleep(3)
+bid_no=[]
+items=[]
+quantity=[]
+depart=[]
+start_date=[]
+start_time=[]
+end_date=[]
+end_time=[]
+#pdf_name=[]
+for i in range(1,11):
+    A=browser.find_element_by_xpath('//*[@id="pagi_content"]/div['+str(i)+']/div[1]/p[1]/a')
+    bid_no.append(A.text)
+    B=browser.find_element_by_xpath('//*[@id="pagi_content"]/div['+str(i)+']/div[2]/p[1]/span')
+    items.append(B.text)
+    C=browser.find_element_by_xpath('//*[@id="pagi_content"]/div['+str(i)+']/div[2]/p[2]/span')
+    quantity.append(C.text)
+    D=browser.find_element_by_xpath('//*[@id="pagi_content"]/div['+str(i)+']/div[3]/p[2]')
+    depart.append(D.text)
+    E=browser.find_element_by_xpath('//*[@id="pagi_content"]/div['+str(i)+']/div[4]/p[1]/span')
+    start_date.append(E.text[:11])
+    start_time.append(E.text[11:])
+    F=browser.find_element_by_xpath('//*[@id="pagi_content"]/div['+str(i)+']/div[4]/p[2]/span')
+    end_date.append(E.text[:11])
+    end_time.append(E.text[11:])
+col_name = ["Bid_no.","items","quantity","depart","start_date","start_time","end_date","end_time"]
+col_data = OrderedDict(zip(col_name,[bid_no,items,quantity,depart,start_date,start_time,end_date,end_time]))
+df = pd.DataFrame(col_data) 
+df.to_csv("former.csv")
 
 
 browser.quit()
-right_table=driver.find_element_by_class_name('table')
-
-A=[]
-B=[]
-C=[]
-D=[]
-E=[]
-F=[]
-G=[]
-
-for row in right_table.find_elements_by_tag_name('tr'):
-    cells = row.find_elements_by_tag_name('td')
-    states = row.find_elements_by_tag_name('th')
-    if len(cells) == 6:
-        A.append(states[0].text.strip())
-        B.append(cells[1].text.strip())
-        C.append(cells[2].text.strip())
-        D.append(cells[3].text.strip())
-        E.append(cells[4].text.strip())
-        F.append(cells[5].text.strip())
-        G.append(cells[5].text.strip())
-
-
-col_name = ["Bid_no."]
-col_data = OrderedDict(zip(col_name,[A,B,C,D,E,F,G]))
-df = pd.DataFrame(col_data) 
-df.to_csv("data/former.csv")
-
-
-driver.quit()
-
+#//*[@id="pagi_content"]/div[1]/div[2]/p[1]/span
+#//*[@id="pagi_content"]/div[2]/div[2]/p[1]/span
+//*[@id="pagi_content"]/div[2]/div[2]/p[2]/span
+//*[@id="pagi_content"]/div[1]/div[2]/p[2]/span
+//*[@id="pagi_content"]/div[3]/div[3]/p[2]
+//*[@id="pagi_content"]/div[3]/div[4]/p[1]/span
