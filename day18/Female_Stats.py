6b1f2b76a45cde85ae9bb86692b29b8d054d5522
kuldeepsinghshekhaawt <kdbannachirana101@gmail.com>
2019-06-27 20:45:31 +0530

    female code ins updated

--- day18/Female_Stats.py
+++ day18/Female_Stats.py
@@ -44,14 +44,13 @@ print(regressor_OLS.summary())
 
 
 
-features = dataset.iloc[:,1].values
+features = dataset.iloc[:,[1]].values
 labels=dataset.iloc[:,0].values
 regressor = LinearRegression()
 #import statsmodels.api as sm #impoprtijng library summary
-features=sm.add_constant(features)
 
-dataset.drop("dadheight",axis=1)
-features_opt=features[:,[0,1]]
+#dataset.drop("dadheight",axis=1)
+features_opt=features[:,0]
 regressor_OLS = sm.OLS(endog = labels, exog = features_opt).fit()
 regressor_OLS.summary()
 
@@ -69,13 +68,40 @@ while(True):
         features_opt=np.delete(features_opt,p_values.argmax(),1)
     else:
         break
-    
+   
 print(regressor_OLS.summary()) 
-a=dataset.iloc[0].mean()
-print(a)
-for height in labels:
-    for  i in features:
-        if (i+1):
-            print(a+1)
-        else:
-            break
+
+
+
+
+
+
+
+
+features = dataset.iloc[:,[2]].values
+labels=dataset.iloc[:,0].values
+regressor = LinearRegression()
+#import statsmodels.api as sm #impoprtijng library summary
+
+#dataset.drop("dadheight",axis=1)
+features_opt=features[:,[0]]
+regressor_OLS = sm.OLS(endog = labels, exog = features_opt).fit()
+regressor_OLS.summary()
+
+from sklearn.cross_validation import train_test_split
+features_train,features_test,labels_train,labels_test=train_test_split(features,labels,test_size=0.2,random_state=0)
+from sklearn.linear_model import LinearRegression
+lm=LinearRegression()
+lm.fit(features_train,labels_train)
+labels_test=lm.predict(features_test)
+print(labels_test)
+while(True):
+    regressor_OLS=sm.OLS(endog=labels,exog=features_opt).fit()
+    p_values=regressor_OLS.pvalues
+    if p_values.max()>.05:
+        features_opt=np.delete(features_opt,p_values.argmax(),1)
+    else:
+        break
+   
+print(regressor_OLS.summary()) 
+  
