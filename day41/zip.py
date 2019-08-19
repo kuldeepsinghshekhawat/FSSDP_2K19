1dcdd149f519d33f4b1431349afc060cfb0ca751
kuldeepsinghshekhaawt <kdbannachirana101@gmail.com>
2019-08-19 18:02:57 +0530

    zip files

---
+++ day41/zip.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 17:45:12 2019
+
+@author: lenovo
+"""
+
+#generator is a simply way of creating a iterator
+#In this section we use zip function
+city=["Delhi","Noida","Jaipur"]
+lang=["Hindi","Java","English"]
+z=list(zip(city,lang))
+print(z)
---
+++ day41/zip1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 17:50:53 2019
+
+@author: lenovo
+"""
+
+college=["Sobhasria Engg.","Mody","Pratap engg."]
+location=["Sikar","Laxamangarh","Palsana"]
+z=tuple(zip(college,location))
+print(z)
